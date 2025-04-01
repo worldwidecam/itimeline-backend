8b823113fdf25e91e7aa58cc87ff6c15e2df7cac
Cam <120047551+worldwidecam@users.noreply.github.com>
2025-04-01 08:47:04 -0700

    Configure for production deployment with Gunicorn WSGI server

--- build.sh
+++ build.sh
@@ -8,7 +8,4 @@ pip install -r requirements.txt
 # Create necessary directories if they don't exist
 mkdir -p instance
 
-# Run any database migrations if needed
-python manage.py db upgrade
-
 # Additional setup steps can be added here
